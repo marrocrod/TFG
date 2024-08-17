514f1108183089ae94fb245d77ed4177b2d0c564
marrocrod <marco-tarifa9@hotmail.com>
2024-08-17 15:46:22 +0200

    Register y configuración static

--- TrabajoFinGrado/main/urls.py
+++ TrabajoFinGrado/main/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
-from .views import home
+from .views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', home, name='home'),
+    path('register/', register_view, name='register_view'),
+    path('register/teacher/', register_teacher, name='register_teacher'),
+    path('register/student/', register_student, name='register_student'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
